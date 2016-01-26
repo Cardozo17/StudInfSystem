0dfe6e9a332ee74734e8ad83aba08996bdd36ad9
Jose Cardozo <jcardozo17@gmail.com>
2016-01-25 22:57:24 -0430

    bootstrap is already working

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # studInfSystemVD
-Student Information System to register and give statistics about students in an specific public school. This is intended to make the data manage easier in the organizations. Statistics will be really helpful for the schools.
-The primary purpose of the project is to fulfill our community service in venezuela, but we are also thinking ahead
+Student Information System to register and give statistics about students in an specific public school. This is intended to make the data management easier in the organizations. Statistics will be really helpful for the schools.
+The primary purpose of the project is to fulfill our community service in Venezuela, but we are also thinking ahead
--- StudInfSystVicDav/resources/views/welcome.blade.php
+++ StudInfSystVicDav/resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
     <head>
         <title>Sistema de Información Estudiantil</title>
 
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" integrity="sha256-7s5uDGW3AHqw6xtJmNNtr+OBRJUlgkNJEo78P4b0yRw= sha512-nNo+yCHEyn0smMxSswnf/OnX6/KwJuZTlNZBjauKhTK0c+zT+q5JOCx0UFhXQ6rJR9jg6Es8gPuD2uZcYDLqSw==" crossorigin="anonymous">
 
         <style>
             html, body {
@@ -47,7 +47,10 @@
                 <button type="button" class="btn btn-danger">Danger</button><br>
                 <button type="button" class="btn btn-link">Link</button><br>
                 <h1>Why does not the boostrap button (color) work here? </h1>
+                <h2>It Works Now! Bastard</h2>
             </div>
         </div>
     </body>
+
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha256-KXn5puMvxCw+dAYznun+drMdG1IFl3agK0p/pqT9KAo= sha512-2e8qq0ETcfWRI4HJBzQiA3UoyFk6tbNyG+qSaIBZLyW9Xf3sWZHN/lxe9fTh1U45DpPf07yj94KsUHHWe4Yk1A==" crossorigin="anonymous"></script>
 </html>
