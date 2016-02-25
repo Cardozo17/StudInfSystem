af1dabc7f020390b4293fd5b0ce07fb48298f48a
Jesus <jesusfrosalesi@gmail.com>
2016-02-25 09:23:10 -0430

    Prueba menu

--- StudInfSystVicDav/resources/views/shared/menu.blade.php
+++ StudInfSystVicDav/resources/views/shared/menu.blade.php
@@ -1,12 +1,17 @@
-	<div class="container-fluid" >
-			<div class= "col-md-12">
-
-				<label>Esto es un elemento del menú</label>
-				<br>
-				<label>Esto es otro elemento del menú</label>
-				<a href="/students/create">Inscribir Estudiante</a>
-				<br>
-				<a href="/students">Ver Estudiantes Inscritos</a>
-
-			</div>
-	</div>				
+<nav>
+    <ul>
+        <!--  First active class -->
+        <li class="{{ Request::is( '/') ? 'active' : '' }}">
+        	<a href="{{ URL::to( '/') }}">Home</a>
+        </li>
+        <li class="{{ Request::is( 'about-us') ? 'active' : '' }}">
+        	<a href="{{ URL::to( 'about-us') }}">About us</a>
+        </li>				
+        <li class="{{ Request::is( 'portfolio') ? 'active' : '' }}">
+        	<a href="{{ URL::to( 'portfolio') }}">Portfolio</a>
+        </li>
+        <li class="{{ Request::is( 'contact') ? 'active' : '' }}">
+        	<a href="{{ URL::to( 'contact') }}">Contact</a>
+        </li>
+    </ul>
+</nav>		
