32dfa6bc7adf8e61e93c69a4c811a87f19ca8f24
Jose Cardozo <jcardozo17@gmail.com>
2017-04-19 23:54:43 -0300

    Csrf added to head

--- StudInfSystVicDav/resources/views/layouts/master.blade.php
+++ StudInfSystVicDav/resources/views/layouts/master.blade.php
@@ -4,6 +4,7 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="csrf-token" content="{{ csrf_token() }}"/>
 
     <title>@yield('title')</title>
 
@@ -28,7 +29,6 @@
 
 </head>
 <body ng-app="SISApp">
-	{{ csrf_field() }}
 	<div class= "row">
 		<div class= "col-md-12">
 			@include('layouts.header')
