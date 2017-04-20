9006491bdd6d208ae002cd8d97177cccf4f5c65f
Jose Cardozo <jcardozo17@gmail.com>
2017-04-19 21:58:54 -0300

    Change on welcome page message

--- StudInfSystVicDav/resources/views/welcome.blade.php
+++ StudInfSystVicDav/resources/views/welcome.blade.php
@@ -23,7 +23,7 @@
                         <h1>Bienvenido al Sistema de Información Estudiantil</h1>
                         <h5> Debe iniciar sesión con una cuenta de usuario validada para poder tener acceso a este sistema.
                             <br> Lo invitamos a iniciar sesión si tiene una cuenta y contraseña asignadas.<br>
-                            En caso de no tener una cuenta y contraseña contrario dirijase con el administrador del sistema para que le sea asignada una sí realmente considera que cuenta con las credenciales para obtener acceso al sistema.
+                            En caso de no tener una cuenta y contraseña dirijase con el administrador del sistema para que le sea asignada una sí realmente considera que cuenta con las credenciales para obtener acceso al sistema.
                             <h5>
                     </div>
                 </div>
