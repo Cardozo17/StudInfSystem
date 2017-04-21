7ae56cc26b79fcc1a1aeeb1de02698077bd44e09
Jose Cardozo <jcardozo17@gmail.com>
2017-04-21 00:44:41 -0300

    Testing

--- StudInfSystVicDav/resources/views/auth/login.blade.php
+++ StudInfSystVicDav/resources/views/auth/login.blade.php
@@ -19,7 +19,6 @@
                     <div class="panel-heading">Identifiquese</div>
                     <div class="panel-body">
                         <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
-                            <input type="hidden" name="_token" value="{{ csrf_token() }}">
                             <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
                                 <label class="col-md-4 control-label">Correo Electrónico</label>
 
--- StudInfSystVicDav/resources/views/welcome.blade.php
+++ StudInfSystVicDav/resources/views/welcome.blade.php
@@ -17,7 +17,6 @@
             <div class="col-md-12">
                 <div class="panel panel-default">
                     <div class="panel-heading">Bienvenido</div>
-                   
                     <div class="panel-body">
                         <h1>Bienvenido al Sistema de Información Estudiantil</h1>
                         <h5> Debe iniciar sesión con una cuenta de usuario validada para poder tener acceso a este sistema.
