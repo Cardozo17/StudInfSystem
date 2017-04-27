15a574dfc0ea1e882f24cf0651a4c8124d2d7da3
Jose Cardozo <jcardozo17@gmail.com>
2017-04-26 21:23:56 -0300

    Testing not auth middleware

--- StudInfSystVicDav/app/Http/routes.php
+++ StudInfSystVicDav/app/Http/routes.php
@@ -59,107 +59,106 @@ Route::group(['middleware' => 'web'], function ()
      //Get Control Parameters Route
      Route::get('getSystemParameters','SystemController@getSystemParameters');
 
-    Route::group(['middleware' => ['auth']], function ()
-{
-    /**********************************************************/
-        Route::get('about', 'HomeController@showAboutUsWindow');
-        Route::get('contact', 'HomeController@showContactWindow');
-    /**********************************************************/
+    //Route::group(['middleware' => ['auth']], function ()
+    //{
+        /**********************************************************/
+            Route::get('about', 'HomeController@showAboutUsWindow');
+            Route::get('contact', 'HomeController@showContactWindow');
+        /**********************************************************/
 
-    /**********************************************************/
-        Route::get('teachers/create', [
-            'middleware' => 'is_adminAdministrativePersonLevel1and2',
-            'uses' => 'TeacherController@showCreateTeacherWindow'
-        ]);
-        Route::get('teachers/assign', [
-            'middleware' => 'is_adminAdministrativePersonLevel1and2',
-            'uses' => 'TeacherController@showAssignTeacherWindow'
-        ]);
-         Route::post('teachers', 'TeacherController@store');
-         Route::post('/teacherById','TeacherController@findTeacherById');
-         Route::post('assignTeacher', 'TeacherController@assignTeacher');
-    /**********************************************************/
+        /**********************************************************/
+            Route::get('teachers/create', [
+                'middleware' => 'is_adminAdministrativePersonLevel1and2',
+                'uses' => 'TeacherController@showCreateTeacherWindow'
+            ]);
+            Route::get('teachers/assign', [
+                'middleware' => 'is_adminAdministrativePersonLevel1and2',
+                'uses' => 'TeacherController@showAssignTeacherWindow'
+            ]);
+             Route::post('teachers', 'TeacherController@store');
+             Route::post('/teacherById','TeacherController@findTeacherById');
+             Route::post('assignTeacher', 'TeacherController@assignTeacher');
+        /**********************************************************/
 
-    /**********************************************************/
-        Route::get('students/create', [
-            'middleware' => 'is_adminTeacherAdminitrativePersonLevel1',
-            'uses' => 'StudentController@showCreateStudentWindow'
-        ]);
-        Route::get('showFindStudent', [
-            'middleware' => 'is_adminTeacherAdminitrativePersonLevel1and2',
-            'uses' => 'StudentController@showFindOneStudentWindow'
-        ]);
-        Route::get('students',[
-            'middleware' => 'is_adminTeacherAdminitrativePersonLevel1and2',
-            'uses'=>'StudentController@index']);
-        Route::get('students/putGrades',[
-            'middleware' => 'is_adminTeacherAdminitrativePersonLevel1and2',
-            'uses'=>'StudentController@showPutGrades']);
-        Route::get('students/list', 'StudentController@listStudents');
-
-        Route::post('/students/listBySectionGrade', 'StudentController@listStudentsBySectionGrade');
-        Route::post('studentById','StudentController@findStudentById');
-        Route::post('students', 'StudentController@store');
-        Route::post('/students/assignGrade', 'StudentController@assignGradeToStudent');
-    /**********************************************************/
-
-    /**********************************************************/
-        Route::get('repStudyConstancy', [
-            'middleware' => 'is_adminAdministrativePersonLevel1and2',
-            'uses' =>'ReportController@showMakeStudyConstancyWindow', 'as' => 'Report1']);
-        Route::get('repCitation', [
-            'middleware' => 'is_adminTeacherAdminitrativePersonLevel1and2',
-            'uses' =>'ReportController@showMakeCitationWindow', 'as' => 'Report2']);
-        Route::get('repAuthorization', [
-            'middleware' => 'is_adminTeacherAdminitrativePersonLevel1and2',
-            'uses' =>'ReportController@showMakeAuthorizationWindow', 'as' => 'Report3']);
-        Route::post('repStudyConstancy', ['uses' =>'ReportController@makeStudyConstancy']);
-        Route::post('repCitation', ['uses' =>'ReportController@makeCitation']);
-        Route::post('repAuthorization', ['uses' =>'ReportController@makeAuthorization']);
-    /**********************************************************/
-
-    /**********************************************************/
-       Route::get('statistics/grades', [
-            'middleware' => 'is_adminAdministrativePersonLevel1and2',
-            'uses' => 'StatisticsController@showGradesStatistics'
-        ]);
-    /**********************************************************/
-
-    /**********************************************************/
-        Route::get('registerUser', [
-            'middleware' => 'is_admin',
-            'uses' => 'Auth\AuthController@showRegistrationForm'
-        ]);
-        Route::get('editUser', [
-            'middleware' => 'is_admin',
-            'uses' => 'Auth\AuthController@showEditUserWindow'
-        ]);
-        Route::get('deleteUser', [
-            'middleware' => 'is_admin',
-            'uses' => 'Auth\AuthController@showDeleteUserWindow'
-        ]);
-        Route::post('registerUser', 'Auth\AuthController@register');
-        Route::post('userByEmail','Auth\AuthController@findUserByEmail');
-        Route::post('editUser','Auth\AuthController@editUser');
-        Route::post('deleteUser','Auth\AuthController@deleteUser');
-    /**********************************************************/
-
-    /**********************************************************/
-        Route::get('studentsInfoBackUp', [
+        /**********************************************************/
+            Route::get('students/create', [
                 'middleware' => 'is_adminTeacherAdminitrativePersonLevel1',
-                'uses' => 'BackUpController@showBackUpWindow'
+                'uses' => 'StudentController@showCreateStudentWindow'
             ]);
-    /**********************************************************/
+            Route::get('showFindStudent', [
+                'middleware' => 'is_adminTeacherAdminitrativePersonLevel1and2',
+                'uses' => 'StudentController@showFindOneStudentWindow'
+            ]);
+            Route::get('students',[
+                'middleware' => 'is_adminTeacherAdminitrativePersonLevel1and2',
+                'uses'=>'StudentController@index']);
+            Route::get('students/putGrades',[
+                'middleware' => 'is_adminTeacherAdminitrativePersonLevel1and2',
+                'uses'=>'StudentController@showPutGrades']);
+            Route::get('students/list', 'StudentController@listStudents');
 
-    /**********************************************************/
-        Route::get('systemParameters', [
+            Route::post('/students/listBySectionGrade', 'StudentController@listStudentsBySectionGrade');
+            Route::post('studentById','StudentController@findStudentById');
+            Route::post('students', 'StudentController@store');
+            Route::post('/students/assignGrade', 'StudentController@assignGradeToStudent');
+        /**********************************************************/
+
+        /**********************************************************/
+            Route::get('repStudyConstancy', [
+                'middleware' => 'is_adminAdministrativePersonLevel1and2',
+                'uses' =>'ReportController@showMakeStudyConstancyWindow', 'as' => 'Report1']);
+            Route::get('repCitation', [
+                'middleware' => 'is_adminTeacherAdminitrativePersonLevel1and2',
+                'uses' =>'ReportController@showMakeCitationWindow', 'as' => 'Report2']);
+            Route::get('repAuthorization', [
+                'middleware' => 'is_adminTeacherAdminitrativePersonLevel1and2',
+                'uses' =>'ReportController@showMakeAuthorizationWindow', 'as' => 'Report3']);
+            Route::post('repStudyConstancy', ['uses' =>'ReportController@makeStudyConstancy']);
+            Route::post('repCitation', ['uses' =>'ReportController@makeCitation']);
+            Route::post('repAuthorization', ['uses' =>'ReportController@makeAuthorization']);
+        /**********************************************************/
+
+        /**********************************************************/
+           Route::get('statistics/grades', [
+                'middleware' => 'is_adminAdministrativePersonLevel1and2',
+                'uses' => 'StatisticsController@showGradesStatistics'
+            ]);
+        /**********************************************************/
+
+        /**********************************************************/
+            Route::get('registerUser', [
                 'middleware' => 'is_admin',
-                'uses' => 'SystemController@showSystemParametersWindow'
+                'uses' => 'Auth\AuthController@showRegistrationForm'
             ]);
-        Route::post('setSystemParameters', 'SystemController@store');
-    /**********************************************************/
-});
+            Route::get('editUser', [
+                'middleware' => 'is_admin',
+                'uses' => 'Auth\AuthController@showEditUserWindow'
+            ]);
+            Route::get('deleteUser', [
+                'middleware' => 'is_admin',
+                'uses' => 'Auth\AuthController@showDeleteUserWindow'
+            ]);
+            Route::post('registerUser', 'Auth\AuthController@register');
+            Route::post('userByEmail','Auth\AuthController@findUserByEmail');
+            Route::post('editUser','Auth\AuthController@editUser');
+            Route::post('deleteUser','Auth\AuthController@deleteUser');
+        /**********************************************************/
 
+        /**********************************************************/
+            Route::get('studentsInfoBackUp', [
+                    'middleware' => 'is_adminTeacherAdminitrativePersonLevel1',
+                    'uses' => 'BackUpController@showBackUpWindow'
+                ]);
+        /**********************************************************/
+
+        /**********************************************************/
+            Route::get('systemParameters', [
+                    'middleware' => 'is_admin',
+                    'uses' => 'SystemController@showSystemParametersWindow'
+                ]);
+            Route::post('setSystemParameters', 'SystemController@store');
+        /**********************************************************/
+    //});
 
     // Logout Routes...
     Route::get('logout', 'Auth\AuthController@logout');
